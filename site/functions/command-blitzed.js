fc03d2810676e8b101025e01ebbe763611d98971
Jason Lengstorf <jason@lengstorf.com>
2020-01-25 10:24:45 -0800

    feat: update stream effects

--- site/functions/command-blitzed.js
+++ site/functions/command-blitzed.js
@@ -1,13 +1,12 @@
 const createHandler = require('@stream-blitz/create-handler');
 
-exports.handler = createHandler(({ extra: { channel } }) => ({
+exports.handler = createHandler(() => ({
   name: 'blitzed',
   message: 'KAPOW YOU GOT BLITZED! MorphinTime',
   description:
-    'Every hero needs a catchphrase. Sometimes everyday doofers need them, too.',
-  sfx:
+    'Every hero needs a catchphrase. Sometimes an everyday doofus needs one, too.',
+  audio:
     'https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/lwj-sfx/blitzed.mp3',
-  gif:
+  image:
     'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_400/v1573512575/lwj-sfx/victory',
-  channel,
 }));
--- site/functions/command-plzhold.js
+++ site/functions/command-plzhold.js
@@ -1,14 +1,13 @@
 const createHandler = require('@stream-blitz/create-handler');
 
-exports.handler = createHandler(({ extra: { channel } }) => ({
+exports.handler = createHandler(() => ({
   name: 'plzhold',
   message: 'SeriousSloth compiling...',
   description:
     'For when the computers aren’t keeping up with our need for constant stimulation.',
-  sfx:
+  audio:
     'https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/lwj-sfx/elevator-music.mp3',
-  gif:
+  image:
     'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_400/v1573512575/lwj-sfx/waiting',
   duration: 10,
-  channel,
 }));
