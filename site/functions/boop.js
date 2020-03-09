f28338d6ce90bdb8935ab1a9d410a46f0aed1367
Jason Lengstorf <jason@lengstorf.com>
2020-03-08 20:02:53 -0700

    feat: better boopin’

--- site/functions/boop.js
+++ site/functions/boop.js
@@ -3,6 +3,6 @@ const createHandler = require('@stream-blitz/create-handler');
 exports.handler = createHandler(() => ({
   name: 'boop',
   message:
-    'jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP',
-  description: 'Spam that boop!',
+    'CorgiDerp PartyHat jlengsBOOP jlengsCorgi CorgiDerp PartyHat jlengsBOOP jlengsCorgi CorgiDerp PartyHat jlengsBOOP jlengsCorgi CorgiDerp PartyHat jlengsBOOP jlengsCorgi CorgiDerp PartyHat jlengsBOOP jlengsCorgi CorgiDerp PartyHat jlengsBOOP jlengsCorgi CorgiDerp PartyHat jlengsBOOP jlengsCorgi CorgiDerp PartyHat jlengsBOOP jlengsCorgi CorgiDerp PartyHat jlengsBOOP jlengsCorgi CorgiDerp PartyHat jlengsBOOP jlengsCorgi CorgiDerp PartyHat jlengsBOOP jlengsCorgi CorgiDerp PartyHat',
+  description: 'Welcome new subscribers with this command. Spam that boop!',
 }));
