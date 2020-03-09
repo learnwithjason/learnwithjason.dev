fe6c0519c3dc3701947fd9dac4ece87d17f15c79
Jason Lengstorf <jason@lengstorf.com>
2020-03-08 19:04:01 -0700

    feat: add !boop command

---
+++ site/functions/boop.js
@@ -0,0 +1,8 @@
+const createHandler = require('@stream-blitz/create-handler');
+
+exports.handler = createHandler(() => ({
+  name: 'boop',
+  message:
+    'jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP jlengsBOOP',
+  description: 'Spam that boop!',
+}));
