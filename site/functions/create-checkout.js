2192e312646356205215e539b473649a45245018
Jason Lengstorf <jason@lengstorf.com>
2020-03-31 10:37:23 -0700

    feat: add a sticker store

---
+++ site/functions/create-checkout.js
@@ -0,0 +1,33 @@
+const stripe = require('stripe')(process.env.STRIPE_API_SECRET);
+const inventory = require('./data/products.json');
+
+exports.handler = async ({ body }) => {
+  const { sku, quantity } = JSON.parse(body);
+  const product = inventory.find(p => p.sku === sku);
+  const validatedQuantity = quantity > 0 && quantity <= 10 ? quantity : 1;
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    billing_address_collection: 'auto',
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA'],
+    },
+    line_items: [
+      {
+        name: product.name,
+        description: product.description,
+        images: [product.image],
+        amount: product.amount,
+        currency: product.currency,
+        quantity: validatedQuantity,
+      },
+    ],
+    success_url: 'https://www.learnwithjason.dev/purchased',
+    cancel_url: 'https://www.learnwithjason.dev/store',
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ sessionId: session.id }),
+  };
+};
