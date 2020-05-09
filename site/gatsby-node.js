4fb041af3a9b54531c3e0c86ba33321def5e38fa
Jason Lengstorf <jason@lengstorf.com>
2020-05-08 22:40:46 -0700

    fix: null loader for Twitch embedder

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,3 +1,18 @@
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-twitch-embed-video/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};
