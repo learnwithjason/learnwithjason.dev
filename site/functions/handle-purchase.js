361f12562b9c88d4e161c88cee0913647910ae01
Jason Lengstorf <jason@lengstorf.com>
2020-05-13 19:32:29 -0700

    feat: enable store webhook handling

--- site/functions/handle-purchase.js
+++ site/functions/handle-purchase.js
@@ -1,31 +1,60 @@
 const stripe = require('stripe')(process.env.STRIPE_API_SECRET);
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+const mail = require('@sendgrid/mail');
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = async ({ headers, body }) => {
-  const sig = headers['stripe-signature'];
-  let event;
   try {
-    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+    const event = stripe.webhooks.constructEvent(
+      body,
+      headers['stripe-signature'],
+      endpointSecret,
+    );
+
+    if (event.type !== 'checkout.session.completed') {
+      return;
+    }
+
+    const order = event.data.object;
+
+    const {
+      line1,
+      line2,
+      city,
+      state,
+      postal_code,
+      country,
+    } = order.shipping.address;
+
+    const items = order.display_items;
+
+    const msg = {
+      to: process.env.FULFILLMENT_EMAIL_ADDRESS,
+      from: 'support@learnwithjason.dev',
+      subject: 'New order from the Learn With Jason store!',
+      text: `
+Items:
+${items.map((item) => `- (${item.quantity}) ${item.custom.name}`).join('\n')}
+
+Shipping Address:
+${order.shipping.name}
+${line1}${line2 !== null ? '\n' + line2 : ''}
+${city}, ${state} ${postal_code}
+${country}
+`,
+    };
+
+    await mail.send(msg);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ received: true }),
+    };
   } catch (error) {
     return {
       statusCode: 400,
       body: `WebHook error: ${error.message}`,
     };
   }
-
-  if (event.type === 'checkout.session.completed') {
-    const session = event.data.object;
-
-    console.log(session);
-  }
-
-  // TODO read out the shipping address
-  // TODO read out the line items
-
-  // TODO send email
-
-  return {
-    statusCode: 200,
-    body: 'todo',
-  };
 };
