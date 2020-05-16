902f0dff477626073a4c7515e5fe63e62a0e8e75
Jason Lengstorf <jason@lengstorf.com>
2020-05-15 20:15:38 -0700

    fix: adjust command to work with updated StreamBlitz handler args

--- site/functions/so.js
+++ site/functions/so.js
@@ -1,15 +1,16 @@
 const createHandler = require('@stream-blitz/create-handler');
 
-exports.handler = createHandler(({ user, message, flags, extra }) => {
-  console.log({ user, message, flags, extra });
-
-  // only broadcasters or mods can shoutout
-  if (!flags.broadcaster && !flags.mod) {
+exports.handler = createHandler(({ author, message }) => {
+  if (
+    !author ||
+    !author.roles.includes('SUBSCRIBER') ||
+    !author.roles.includes('BROADCASTER')
+  ) {
     return;
   }
 
   const parts = message.split(' ');
-  const soTarget = parts.find(part => part.startsWith('@'));
+  const soTarget = parts.find((part) => part.startsWith('@'));
   if (!soTarget) {
     return;
   }
