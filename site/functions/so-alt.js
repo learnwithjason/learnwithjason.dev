9881969974b53f389046fd1a0b4404234c1a96f5
Jason Lengstorf <jason@lengstorf.com>
2020-05-17 23:00:40 -0700

    wip: new filename to see if it’s cursed

---
+++ site/functions/so-alt.js
@@ -0,0 +1,24 @@
+const createHandler = require('@stream-blitz/create-handler');
+
+exports.handler = createHandler(({ author, arguments }) => {
+  if (
+    !author ||
+    !author.roles.includes('SUBSCRIBER') ||
+    !author.roles.includes('BROADCASTER')
+  ) {
+    return;
+  }
+
+  const soTarget = arguments.find((part) => part.startsWith('@'));
+  if (!soTarget) {
+    return;
+  }
+
+  const twitchLink = `https://twitch.tv/${soTarget.replace('@', '')}`;
+
+  return {
+    name: 'so2',
+    message: `▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ Please check out ${soTarget} at ${twitchLink} jlengsHolyBucket jlengsBeardy ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬`,
+    description: 'Send a shout-out to someone in chat! `!so @username`',
+  };
+});
