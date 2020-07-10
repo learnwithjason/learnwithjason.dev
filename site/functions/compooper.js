a4f380f9b9927d52476dbfaa9d5feef08adeb1c0
Jason Lengstorf <jason@lengstorf.com>
2020-07-10 16:50:47 -0700

    fix: get the right sound file in for !compooper

--- site/functions/compooper.js
+++ site/functions/compooper.js
@@ -2,10 +2,10 @@ const createHandler = require('@stream-blitz/create-handler');
 
 exports.handler = createHandler(() => ({
   name: 'compooper',
-  message: 'C’mon lil’ compooper! LuvHearts',
+  message: 'C’mon lil’ compooper!',
   description: 'C’mon lil’ compooper!',
   audio:
-    'https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/lwj-sfx/code.mp3',
+    'https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1590110036/lwj-sfx/compooper.mp3',
   image:
     'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_400/v1573512575/lwj-sfx/please',
 }));
