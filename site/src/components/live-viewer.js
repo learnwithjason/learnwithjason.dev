b6e1933809e04de74dc5f88fac6c8bd2b28cdad8
Jason Lengstorf <jason@lengstorf.com>
2020-07-10 17:13:05 -0700

    wip: test embed fix

--- site/src/components/live-viewer.js
+++ site/src/components/live-viewer.js
@@ -6,7 +6,7 @@ import { Link } from 'gatsby';
 import Heading from './heading';
 import { useStreamDetails } from '../hooks/use-stream-details';
 
-const CHANNEL_NAME = 'jlengstorf';
+const CHANNEL_NAME = 'lcs';
 
 export const LiveViewer = () => {
   const { loading, live } = useStreamDetails(CHANNEL_NAME);
@@ -59,7 +59,7 @@ export const LiveViewer = () => {
       >
         <ReactTwitchEmbedVideo
           channel={CHANNEL_NAME}
-          parent={['learnwithjason.dev']}
+          parent={['www.learnwithjason.dev']}
         />
       </div>
       <iframe
