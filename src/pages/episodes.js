98e67b07d5075059ca9ede558f47d92c556df043
Jason Lengstorf <jason@lengstorf.com>
2020-12-19 15:00:28 -0800

    feat: copy tweaks + Stripe URLs

--- src/pages/episodes.js
+++ src/pages/episodes.js
@@ -5,7 +5,10 @@ export default function Episodes({ episodes }) {
   return (
     <Fragment>
       <header class="block hero">
-        <h1>Episodes</h1>
+        <h1>All Episodes</h1>
+        <p>
+          Browse all {episodes.length} episodes of <em>Learn With Jason</em>.
+        </p>
       </header>
       <EpisodeList episodes={episodes} />
     </Fragment>
