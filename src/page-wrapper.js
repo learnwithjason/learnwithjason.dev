08b3e5c9723003bef4dca12ee5f4a6330e3cd605
Jason Lengstorf <jason@lengstorf.com>
2020-12-18 23:49:04 -0800

    feat: favicon

--- src/page-wrapper.js
+++ src/page-wrapper.js
@@ -86,6 +86,29 @@ export default function PageWrapper(props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <title>{title}</title>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#c10b7e" />
+        <meta name="msapplication-TileColor" content="#201d29" />
+        <meta name="theme-color" content="#ffffff"></meta>
+
         <meta name="description" content={description} />
         <meta name="image" content={image} />
 
