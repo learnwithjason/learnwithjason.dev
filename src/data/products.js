8cbd4dbcebd409c0b2855fd469173ecdb6e26c0b
Jason Lengstorf <jason@lengstorf.com>
2020-12-20 22:25:13 -0800

    wip: store + cart

--- src/data/products.js
+++ src/data/products.js
@@ -1,110 +1,92 @@
 export const products = [
   {
+    id: 'price_0Hzz88C9XkV19gkOrokJG6l1',
+    name: '8 Sticker Pack',
+    description: 'One each of all available stickers — holy buckets!',
+    dimensions: false,
+    amount: 1500,
+    currency: 'USD',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/sticker-pack-v2.png',
+  },
+  {
     id: 'price_0Hzz7OC9XkV19gkOXh49WOoU',
-    name: 'Sticker 1',
-    amount: 300,
+    name: 'Bearded Corgi',
+    description: 'It’s a party corgi, but with a beard!',
+    dimensions: '1.5”h x 1.59”',
+    amount: 400,
     currency: 'USD',
     image:
       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/bearded-corgi.png',
   },
   {
-    id: 'price_0Hzz88C9XkV19gkOrokJG6l1',
-    name: 'Sticker 2',
-    amount: 300,
+    id: 'price_0Hzz7OC9XkV19gkOXh49WOoU',
+    name: 'Rubber Corgi',
+    description: 'This rubber corgi will listen to your problems.',
+    dimensions: '1.92”h x 2”',
+    amount: 400,
     currency: 'USD',
     image:
-      'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/sticker-pack-v2.png',
+      'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/rubber-corgi.png',
+  },
+  {
+    id: 'price_0Hzz7OC9XkV19gkOXh49WOoU',
+    name: 'Corgi Pal',
+    description: 'Add a corgi pal to your collection!',
+    dimensions: '1.59”h x 2”',
+    amount: 400,
+    currency: 'USD',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/corgi-pal.png',
+  },
+  {
+    id: 'price_0Hzz7OC9XkV19gkOXh49WOoU',
+    name: 'BOOP',
+    description: 'Do you wanna get booped on the brain?',
+    dimensions: '2” x 1.82”',
+    amount: 400,
+    currency: 'USD',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/boop.png',
+  },
+  {
+    id: 'price_0Hzz7OC9XkV19gkOXh49WOoU',
+    name: 'Play Until It Pays',
+    description: 'Make it because it’s fun. The rest will follow.',
+    dimensions: '2” x 1.75”',
+    amount: 400,
+    currency: 'USD',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/play-until-it-pays.png',
+  },
+  {
+    id: 'price_0Hzz7OC9XkV19gkOXh49WOoU',
+    name: 'BEEP BOOP',
+    description: 'Boop, but beepier because it’s Morse code.',
+    dimensions: '2” x 1.75”',
+    amount: 400,
+    currency: 'USD',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/beep-boop.png',
+  },
+  {
+    id: 'price_0Hzz7OC9XkV19gkOXh49WOoU',
+    name: 'LWJ Logo',
+    description: 'Boop crew membership badge.',
+    dimensions: '1.75”w x 2”',
+    amount: 400,
+    currency: 'USD',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/learn-with-jason.png',
+  },
+  {
+    id: 'price_0Hzz7OC9XkV19gkOXh49WOoU',
+    name: 'Stream Blitz',
+    description: 'Have more fun on Twitch with Stream Blitz!',
+    dimensions: '1.82”w x 2”',
+    amount: 400,
+    currency: 'USD',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/stream-blitz.png',
   },
 ];
-// export const products = [
-//   {
-//     sku: 'LWJ008',
-//     name: '8 Sticker Pack',
-//     description: 'One each of all available stickers — holy buckets!',
-//     dimensions: '',
-//     image:
-//       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/sticker-pack-v2.png',
-//     amount: 1500,
-//     currency: 'USD',
-//   },
-//   {
-//     sku: 'LWJ001',
-//     name: 'Bearded Corgi',
-//     description: 'It’s a party corgi, but with a beard!',
-//     dimensions: '1.5”h x 1.59”',
-//     image:
-//       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/bearded-corgi.png',
-//     amount: 300,
-//     currency: 'USD',
-//   },
-//   {
-//     sku: 'LWJ009',
-//     name: 'Rubber Corgi',
-//     description: 'This rubber corgi will listen to your problems.',
-//     dimensions: '1.92”h x 2”',
-//     image:
-//       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/rubber-corgi.png',
-//     amount: 300,
-//     currency: 'USD',
-//   },
-//   {
-//     sku: 'LWJ010',
-//     name: 'Corgi Pal',
-//     description: 'Add a corgi pal to your collection!',
-//     dimensions: '1.59”h x 2”',
-//     image:
-//       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/corgi-pal.png',
-//     amount: 300,
-//     currency: 'USD',
-//   },
-//   {
-//     sku: 'LWJ002',
-//     name: 'BOOP',
-//     description: 'Do you wanna get booped on the brain?',
-//     dimensions: '2” x 1.82”',
-//     image:
-//       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/boop.png',
-//     amount: 300,
-//     currency: 'USD',
-//   },
-//   {
-//     sku: 'LWJ003',
-//     name: 'Play Until It Pays',
-//     description: 'Make it because it’s fun. The rest will follow.',
-//     dimensions: '2” x 1.75”',
-//     image:
-//       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/play-until-it-pays.png',
-//     amount: 300,
-//     currency: 'USD',
-//   },
-//   {
-//     sku: 'LWJ004',
-//     name: 'BEEP BOOP',
-//     description: 'Boop, but beepier because it’s Morse code.',
-//     dimensions: '2” x 1.75”',
-//     image:
-//       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/beep-boop.png',
-//     amount: 300,
-//     currency: 'USD',
-//   },
-//   {
-//     sku: 'LWJ005',
-//     name: 'LWJ Logo',
-//     description: 'Boop crew membership badge.',
-//     dimensions: '1.75”w x 2”',
-//     image:
-//       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585784276/lwj/store/learn-with-jason.png',
-//     amount: 300,
-//     currency: 'USD',
-//   },
-//   {
-//     sku: 'LWJ011',
-//     name: 'Stream Blitz',
-//     description: 'Have more fun on Twitch with Stream Blitz!',
-//     dimensions: '1.82”w x 2”',
-//     image:
-//       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/stream-blitz.png',
-//     amount: 300,
-//     currency: 'USD',
-//   },
-// ];
--- src/pages/store.js
+++ src/pages/store.js
@@ -1,9 +1,17 @@
-import { h } from 'preact';
+import { h, Fragment } from 'preact';
 import { useState } from 'preact/hooks';
+import { Helmet } from 'react-helmet';
 import { loadStripe } from '@stripe/stripe-js';
 import { products } from '../data/products.js';
 
 const stripePromise = loadStripe(process.env.TOAST_STRIPE_PUBLISHABLE_KEY);
+
+function format({ amount, currency }) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency,
+  }).format((amount / 100).toFixed(2));
+}
 
 export default function Store() {
   const [cart, setCart] = useState([]);
@@ -31,41 +39,86 @@ export default function Store() {
   }
 
   return (
-    <section class="block store">
-      <div class="products">
-        {products.map((product) => {
-          function addToCart() {
-            const isInCart = cart.some(({ price }) => price === product.id);
+    <Fragment>
+      <Helmet>
+        <link rel="stylesheet" href="/styles/store.css" />
+      </Helmet>
+      <section class="block store">
+        <div class="products">
+          {products.map((product) => {
+            function addToCart() {
+              const isInCart = cart.some(({ price }) => price === product.id);
 
-            const newCart = isInCart
-              ? cart.map((item) => {
-                  if (item.price !== product.id) {
-                    return item;
-                  }
+              const newCart = isInCart
+                ? cart.map((item) => {
+                    if (item.price !== product.id) {
+                      return item;
+                    }
 
-                  return {
-                    price: item.price,
-                    quantity: item.quantity + 1,
-                  };
-                })
-              : [...cart, { price: product.id, quantity: 1 }];
+                    return {
+                      price: item.price,
+                      quantity: item.quantity + 1,
+                    };
+                  })
+                : [...cart, { price: product.id, quantity: 1 }];
 
-            setCart(newCart);
-          }
+              setCart(newCart);
+            }
 
-          return (
-            <div class="product" key={product.id}>
-              <h2>{product.name}</h2>
-              <button onClick={addToCart}>Add To Cart</button>
-            </div>
-          );
-        })}
-      </div>
-      <div class="cart">
-        <p>TODO show cart</p>
-        <pre>{JSON.stringify(cart, null, 2)}</pre>
-        <button onClick={handleSubmit}>Check Out</button>
-      </div>
-    </section>
+            const formattedPrice = format({
+              amount: product.amount,
+              currency: product.currency,
+            });
+
+            return (
+              <div class="product" key={product.id}>
+                <img src={product.image} alt={product.name} />
+                <h2>{product.name}</h2>
+                <p>
+                  {product.description}{' '}
+                  {product.dimensions && `(dimensions: ${product.dimensions})`}
+                </p>
+                <p class="price">{formattedPrice}</p>
+                <button onClick={addToCart}>Add To Cart</button>
+              </div>
+            );
+          })}
+        </div>
+        <aside class="cart">
+          <h3>Your Cart</h3>
+          {cart.length === 0 ? (
+            <p class="empty">
+              your cart is empty! maybe boop it with a sticker or two?
+            </p>
+          ) : (
+            <ul class="cart-items">
+              {cart.map((item) => {
+                const cartItem = products.find((p) => p.id === item.price);
+
+                return (
+                  <li key={cartItem.id}>
+                    <span class="cart-item-name">{cartItem.name}</span> &times;{' '}
+                    <span class="cart-item-quantity">{item.quantity}</span>
+                  </li>
+                );
+              })}
+              <li class="subtotal">
+                {format({
+                  amount: cart.reduce((total, item) => {
+                    const { amount } = products.find(
+                      (p) => p.id === item.price,
+                    );
+
+                    return (total += amount * item.quantity);
+                  }, 0),
+                  currency: 'usd',
+                })}
+              </li>
+              <button onClick={handleSubmit}>Check Out</button>
+            </ul>
+          )}
+        </aside>
+      </section>
+    </Fragment>
   );
 }
