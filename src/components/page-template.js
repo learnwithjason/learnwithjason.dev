0247191550c01ea228b6c0cabe64d7ddc368b592
Jason Lengstorf <jason@lengstorf.com>
2020-12-29 12:02:50 -0800

    feat: add sharing buttons for browsers that support the API

---
+++ src/components/page-template.js
@@ -0,0 +1,20 @@
+/** @jsx h */
+import { h, Fragment } from 'preact';
+import { Helmet } from 'react-helmet';
+
+export function PageTemplate({ title, description, children }) {
+  return (
+    <Fragment>
+      <Helmet>
+        <link rel="stylesheet" href="/styles/post.css" />
+      </Helmet>
+      <header class="block hero">
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </header>
+      <section class="block">
+        <div class="post-content">{children}</div>
+      </section>
+    </Fragment>
+  );
+}
