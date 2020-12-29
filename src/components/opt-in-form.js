022260732ebdf48e9207bccbbcd46bbabb19948e
Jason Lengstorf <jason@lengstorf.com>
2020-12-29 12:02:33 -0800

    feat: add opt-in form

---
+++ src/components/opt-in-form.js
@@ -0,0 +1,36 @@
+import { h, Fragment } from 'preact';
+
+export function OptInForm({ heading = 'Build better web apps', children }) {
+  return (
+    <aside class="opt-in">
+      <form action="/api/subscribe" method="POST">
+        <h2>{heading}</h2>
+        {!children ? (
+          <Fragment>
+            <p>
+              I spend a lot of time thinking about how to{' '}
+              <strong>
+                build web experiences that are fast, secure, maintainable,
+                scalable, and fun to build.
+              </strong>
+            </p>
+            <p>
+              Join my newsletter and I’ll boop you on the brain what I’ve
+              learned about building modern web apps.
+            </p>
+          </Fragment>
+        ) : (
+          children
+        )}
+
+        <label for="firstName">First Name</label>
+        <input type="text" name="firstName" id="firstName" />
+
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" />
+
+        <button>Subscribe</button>
+      </form>
+    </aside>
+  );
+}
