3e944283c7d544218d8cb742daf0ea99eb0aa3bf
Jason Lengstorf <jason@lengstorf.com>
2020-12-29 19:59:29 -0800

    feat: use live product data

--- src/data/products.js
+++ src/data/products.js
@@ -1,16 +1,16 @@
 export const products = [
   {
-    id: 'price_0I29dfC9XkV19gkO2IXZwi9E',
+    id: 'price_0I3uQsC9XkV19gkOFn0JL8qm',
     name: '8 Sticker Pack',
     description: 'One each of all available stickers — holy buckets!',
     dimensions: false,
-    amount: 1500,
+    amount: 1600,
     currency: 'USD',
     image:
       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/sticker-pack-v2.png',
   },
   {
-    id: 'price_0I2ADTC9XkV19gkOJT94Xa1Y',
+    id: 'price_0I3uQwC9XkV19gkOWfB2Ivwc',
     name: 'Bearded Corgi Sticker',
     description: 'It’s a party corgi, but with a beard!',
     dimensions: '1.5”h x 1.59”',
@@ -20,7 +20,7 @@ export const products = [
       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/bearded-corgi.png',
   },
   {
-    id: 'price_0I2AEvC9XkV19gkOVYQog9iu',
+    id: 'price_0I3uQnC9XkV19gkOAODYKVJJ',
     name: 'Rubber Corgi Sticker',
     description: 'This rubber corgi will listen to your problems.',
     dimensions: '1.92”h x 2”',
@@ -30,7 +30,7 @@ export const products = [
       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/rubber-corgi.png',
   },
   {
-    id: 'price_0I2AFdC9XkV19gkOiWdvZztr',
+    id: 'price_0I3uQiC9XkV19gkOutl9SNFE',
     name: 'Corgi Pal Sticker',
     description: 'Add a corgi pal to your collection!',
     dimensions: '1.59”h x 2”',
@@ -40,7 +40,7 @@ export const products = [
       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/lwj/store/corgi-pal.png',
   },
   {
-    id: 'price_0I2AGBC9XkV19gkO6Vrfye8f',
+    id: 'price_0I3uQdC9XkV19gkO5rqOXAbM',
     name: 'BOOP Sticker',
     description: 'Do you wanna get booped on the brain?',
     dimensions: '2” x 1.82”',
@@ -50,7 +50,7 @@ export const products = [
       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/boop.png',
   },
   {
-    id: 'price_0I2AGoC9XkV19gkOqB0eLCbG',
+    id: 'price_0I3uQPC9XkV19gkOrDZO4KtP',
     name: 'Play Until It Pays Sticker',
     description: 'Make it because it’s fun. The rest will follow.',
     dimensions: '2” x 1.75”',
@@ -60,7 +60,7 @@ export const products = [
       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/play-until-it-pays.png',
   },
   {
-    id: 'price_0I2AHNC9XkV19gkOds4bc0HG',
+    id: 'price_0I3uQLC9XkV19gkOGwCMChIs',
     name: 'BEEP BOOP Sticker',
     description: 'Boop, but beepier because it’s Morse code.',
     dimensions: '2” x 1.75”',
@@ -70,7 +70,7 @@ export const products = [
       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/beep-boop.png',
   },
   {
-    id: 'price_0I2AHtC9XkV19gkOEG265EEt',
+    id: 'price_0I3uQGC9XkV19gkO021R82eW',
     name: 'LWJ Logo Sticker',
     description: 'Boop crew membership badge.',
     dimensions: '1.75”w x 2”',
@@ -80,7 +80,7 @@ export const products = [
       'https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300/v1585764654/lwj/store/learn-with-jason.png',
   },
   {
-    id: 'price_0I2AK7C9XkV19gkOmzyosqdi',
+    id: 'price_0I3uQAC9XkV19gkOwJv7PoeV',
     name: 'Stream Blitz Sticker',
     description: 'Have more fun on Twitch with Stream Blitz!',
     dimensions: '1.82”w x 2”',
