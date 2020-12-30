8d50622dd1eb43042c3612a41a858152d0605ce5
Jason Lengstorf <jason@lengstorf.com>
2020-12-29 20:22:50 -0800

    fix: update description

--- functions/haxxors.js
+++ functions/haxxors.js
@@ -2,7 +2,7 @@ const createHandler = require('@stream-blitz/create-handler');
 
 exports.handler = createHandler({
   name: 'haxxors',
-  description: 'Should we focus up?',
+  description: 'for when the chat starts hackin’',
   handler: () => ({
     message: 'MorphinTime no0o0o0 my c0d3z y0u h4x0rz! NotLikeThis',
     audio:
