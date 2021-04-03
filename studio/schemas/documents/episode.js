2899de94d9fae5e6423fee0c86035be0b3cf4c23
Jason Lengstorf <jason@lengstorf.com>
2021-04-02 20:23:19 -0700

    feat: add Sanity config

---
+++ studio/schemas/documents/episode.js
@@ -0,0 +1,101 @@
+export default {
+  name: 'episode',
+  type: 'document',
+  title: 'Episode',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      title: 'Hide this episode on the website',
+      name: 'hidden',
+      type: 'boolean',
+      options: { layout: 'checkbox' },
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      description: 'What should the URL-friendly name of this episode be?',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'image',
+      type: 'episodeImage',
+      title: 'Image',
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Episode Description',
+      description: 'For Google SEO, previews, etc. — a brief overview.',
+    },
+    {
+      name: 'date',
+      type: 'datetime',
+      title: 'Episode Date',
+    },
+    {
+      name: 'guest',
+      title: 'Guest(s)',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'guest' }] }],
+    },
+    {
+      name: 'host',
+      title: 'Host',
+      type: 'reference',
+      to: [{ type: 'guest' }],
+    },
+    {
+      name: 'youtubeID',
+      type: 'string',
+      title: 'YouTube ID',
+      description: 'Just the ID, not the full URL.',
+    },
+    {
+      name: 'demo',
+      type: 'url',
+      title: 'Demo URL',
+      description: 'Where can people see the result of this episode online?',
+    },
+    {
+      name: 'repo',
+      type: 'url',
+      title: 'Repo URL',
+      description: 'Where can people see the source code?',
+    },
+    {
+      name: 'links',
+      type: 'array',
+      title: 'Links and Resources',
+      description: 'Links to anything that was mentioned during the episode.',
+      of: [{ type: 'url' }],
+    },
+    {
+      title: 'Feature this episode',
+      name: 'featured',
+      type: 'boolean',
+      options: { layout: 'checkbox' },
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'tags',
+    },
+    {
+      name: 'transcript',
+      type: 'markdown',
+      title: 'Transcript',
+      description: 'Get this from rev.com as a .txt file.',
+    },
+  ],
+  initialValue: () => ({
+    host: { _type: 'reference', _ref: '1c607bb5-519b-49b6-9da9-5ddc424aa44c' },
+  }),
+};
---
+++ studio/schemas/documents/guest.js
@@ -0,0 +1,23 @@
+export default {
+  name: 'guest',
+  type: 'document',
+  title: 'Guest',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+    {
+      name: 'twitter',
+      type: 'string',
+      title: 'Twitter',
+      description: 'Just the username, no @ or URL (e.g. “jlengstorf”).',
+    },
+    {
+      name: 'guestImage',
+      type: 'image',
+      title: 'Guest Image',
+    },
+  ],
+};
