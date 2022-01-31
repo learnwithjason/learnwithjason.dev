567a625d5c8a0d8191c27be9e78d28857bee1d60
Jason Lengstorf <jason@lengstorf.com>
2022-01-30 21:20:13 -0800

    feat: partially done porting blogs

--- site/app/routes/__mdx/blog.jsx
+++ site/app/routes/__mdx/blog.jsx
@@ -1,7 +1,9 @@
 import { Outlet } from 'remix';
+import prismStyles from 'prismjs/plugins/diff-highlight/prism-diff-highlight.css';
 import styles from '~/styles/post.css';
 
 export const links = () => [
+  { rel: 'stylesheet', href: prismStyles },
   { rel: 'stylesheet', href: '/styles/theme.css' },
   { rel: 'stylesheet', href: styles },
 ];
