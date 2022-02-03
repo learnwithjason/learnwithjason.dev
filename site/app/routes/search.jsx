a00dec2e8e2ee7b686e57b4a3a94227868023904
Jason Lengstorf <jason@lengstorf.com>
2022-02-03 12:57:53 -0800

    fix: get Algolia autocomplate working
    Co-authored-by: Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>

--- site/app/routes/search.jsx
+++ site/app/routes/search.jsx
@@ -1,11 +1,6 @@
-import { useEffect } from 'react';
 import algoliasearch from 'algoliasearch/lite';
-import { autocomplete, getAlgoliaResults } from '@algolia/autocomplete-js';
-
-const searchClient = algoliasearch(
-  'MU9BHW5MNS',
-  '4a0927c9a6b57b94c6b7601a3cc7c41f',
-);
+import { getAlgoliaResults } from '@algolia/autocomplete-js';
+import { Autocomplete } from '~/components/autocomplete';
 
 export const links = () => {
   return [
@@ -16,88 +11,12 @@ export const links = () => {
   ];
 };
 
+const searchClient = algoliasearch(
+  'MU9BHW5MNS',
+  '4a0927c9a6b57b94c6b7601a3cc7c41f',
+);
+
 export default function Search() {
-  useEffect(() => {
-    const search = autocomplete({
-      container: '#autocomplete',
-      placeholder: 'Search for episodes',
-      getSources({ query }) {
-        return [
-          {
-            sourceId: 'episodes',
-            getItemUrl({ item }) {
-              return `https://www.learnwithjason.dev${item.url}`;
-            },
-            getItems() {
-              return getAlgoliaResults({
-                searchClient,
-                queries: [
-                  {
-                    indexName:
-                      'netlify_c55763f8-efc8-4ed9-841a-186a011ed84b_main_all',
-                    query,
-                    params: {
-                      hitsPerPage: 5,
-                    },
-                  },
-                ],
-              });
-            },
-            templates: {
-              item({ item, components }) {
-                return (
-                  <a
-                    class="aa-ItemLink"
-                    href={`https://www.learnwithjason.dev${item.url}`}
-                  >
-                    <div class="aa-ItemContent">
-                      <div class="aa-ItemIcon">
-                        <img
-                          src={item.image}
-                          alt={item.title}
-                          width="40"
-                          height="40"
-                        />
-                      </div>
-                      <div class="aa-ItemContentBody">
-                        <div class="aa-ItemContentTitle">
-                          <components.Highlight hit={item} attribute="title" />
-                        </div>
-                        <div class="aa-ItemContentDescription">
-                          <components.Snippet hit={item} attribute="content" />
-                        </div>
-                      </div>
-                    </div>
-                    <div class="aa-ItemActions">
-                      <button
-                        class="aa-ItemActionButton aa-DesktopOnly aa-ActiveOnly"
-                        type="button"
-                        title="Select"
-                      >
-                        <svg
-                          viewBox="0 0 24 24"
-                          width="20"
-                          height="20"
-                          fill="currentColor"
-                        >
-                          <path d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z" />
-                        </svg>
-                      </button>
-                    </div>
-                  </a>
-                );
-              },
-            },
-          },
-        ];
-      },
-    });
-
-    return () => {
-      search.destroy();
-    };
-  }, []);
-
   return (
     <>
       <section className="block">
@@ -107,7 +26,86 @@ export default function Search() {
           posts using this search field.
         </p>
 
-        <div id="autocomplete" className="search-box-wrapper"></div>
+        <Autocomplete
+          placeholder={'Search for episodes'}
+          getSources={({ query }) => {
+            return [
+              {
+                sourceId: 'episodes',
+                getItemUrl({ item }) {
+                  return `https://www.learnwithjason.dev${item.url}`;
+                },
+                getItems() {
+                  return getAlgoliaResults({
+                    searchClient,
+                    queries: [
+                      {
+                        indexName:
+                          'netlify_c55763f8-efc8-4ed9-841a-186a011ed84b_main_all',
+                        query,
+                        params: {
+                          hitsPerPage: 5,
+                        },
+                      },
+                    ],
+                  });
+                },
+                templates: {
+                  item({ item, components }) {
+                    console.log({ item });
+                    return (
+                      <a
+                        className="aa-ItemLink"
+                        href={`https://www.learnwithjason.dev${item.url}`}
+                      >
+                        <div className="aa-ItemContent">
+                          <div className="aa-ItemIcon">
+                            <img
+                              src={item.image}
+                              alt={item.title}
+                              width="40"
+                              height="40"
+                            />
+                          </div>
+                          <div className="aa-ItemContentBody">
+                            <div className="aa-ItemContentTitle">
+                              <components.Highlight
+                                hit={item}
+                                attribute="title"
+                              />
+                            </div>
+                            <div className="aa-ItemContentDescription">
+                              <components.Snippet
+                                hit={item}
+                                attribute="content"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="aa-ItemActions">
+                          <button
+                            className="aa-ItemActionButton aa-DesktopOnly aa-ActiveOnly"
+                            type="button"
+                            title="Select"
+                          >
+                            <svg
+                              viewBox="0 0 24 24"
+                              width="20"
+                              height="20"
+                              fill="currentColor"
+                            >
+                              <path d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z" />
+                            </svg>
+                          </button>
+                        </div>
+                      </a>
+                    );
+                  },
+                },
+              },
+            ];
+          }}
+        />
 
         <p>
           <small>
