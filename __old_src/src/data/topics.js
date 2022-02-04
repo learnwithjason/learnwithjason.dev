110d226f7bd8dae2e5a9f1a706f56bd41433b230
Jason Lengstorf <jason@lengstorf.com>
2022-02-03 19:09:48 -0800

    chore: remove old src

--- __old_src/src/data/topics.js
+++
@@ -1,6 +0,0 @@
-export const topics = [
-  { title: 'Let’s Learn! Intro Sessions', tag: 'lets-learn' },
-  { title: 'Animation on the Web', tag: 'animation' },
-  { title: 'Design and User Experience', tag: 'design' },
-  { title: 'Serverless Functions', tag: 'serverless' },
-];
