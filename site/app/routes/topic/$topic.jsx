b0ba9d31e2eff2af3a915ddd45ea69fb59424355
Jason Lengstorf <jason@lengstorf.com>
2022-02-05 21:18:13 -0800

    feat: add topic pages

---
+++ site/app/routes/topic/$topic.jsx
@@ -0,0 +1,52 @@
+import { Fragment } from 'react';
+import { useLoaderData, useParams } from 'remix';
+
+import { EpisodeList } from '~/components/episode-list.jsx';
+import { IconArrow } from '~/components/icon-arrow.jsx';
+import { getTeacher } from '~/util/get-teacher.js';
+import { loadAllEpisodes } from '~/util/load-all-episodes.server.js';
+
+export const topicDescriptions = [
+  { title: 'Let’s Learn! Intro Sessions', tag: 'lets-learn' },
+  { title: 'Animation on the Web', tag: 'animation' },
+  { title: 'Design and User Experience', tag: 'design' },
+  { title: 'Serverless Functions', tag: 'serverless' },
+];
+
+export const loader = async ({ params }) => {
+  console.log({ params });
+  const { topic } = params;
+  const topicInfo = topicDescriptions.find((t) => t.tag === topic);
+  const episodes = await loadAllEpisodes();
+
+  return {
+    topic: topicInfo,
+    episodes: episodes
+      .filter((ep) => (ep.tags ?? []).some((tag) => tag.value === topic))
+      .map((episode) => {
+        const host = getTeacher([episode.host]);
+        const teacher = getTeacher(episode.guest);
+
+        return { ...episode, host, teacher };
+      }),
+  };
+};
+
+export const meta = ({ data }) => {
+  return {
+    title: data.topic.title,
+  };
+};
+
+export default function Episodes() {
+  const { topic, episodes } = useLoaderData();
+
+  return (
+    <Fragment>
+      <header className="block hero">
+        <h1>{topic.title}</h1>
+      </header>
+      <EpisodeList episodes={episodes} />
+    </Fragment>
+  );
+}
