9f15f409128980ccf465bb1d37baa2ac9a84e48c
Jason Lengstorf <jason@lengstorf.com>
2022-02-20 22:35:57 -0800

    fix: make sure episodes have all required fields

--- site/app/util/load-all-episodes.server.js
+++ site/app/util/load-all-episodes.server.js
@@ -28,7 +28,40 @@ export async function loadAllEpisodes() {
     ...episodes4,
     ...episodes5,
     ...episodes6,
-  ].filter((e) => e.youtubeID !== null);
+  ]
+    .filter((e) => e.youtubeID !== null)
+    .map((episode) => ({
+      _id: episode._id,
+      date: episode.date,
+      title: episode.title,
+      description: episode.description,
+      demo: episode.demo ?? false,
+      repo: episode.repo ?? false,
+      guest: episode.guest ?? [
+        {
+          guestImage: {
+            asset: {
+              url: 'https://cdn.sanity.io/images/vnkupgyb/production/7d8835955821f584df0b89ab72d2d83799139bb7-660x660.jpg',
+            },
+          },
+          name: 'Jason Lengstorf',
+          twitter: 'jlengstorf',
+        },
+      ],
+      host: episode.host ?? {
+        guestImage: {
+          asset: {
+            url: 'https://cdn.sanity.io/images/vnkupgyb/production/7d8835955821f584df0b89ab72d2d83799139bb7-660x660.jpg',
+          },
+        },
+        name: 'Jason Lengstorf',
+        twitter: 'jlengstorf',
+      },
+      links: episode.links ?? [],
+      slug: episode.slug,
+      youtubeID: episode.youtubeID,
+      tags: episode.tags ?? [],
+    }));
 
   return episodes;
 }
