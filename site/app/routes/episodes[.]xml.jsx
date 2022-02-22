ecfde214e318c151cf42da91640784a0bf14be33
Jason Lengstorf <jason@lengstorf.com>
2022-02-21 17:37:05 -0800

    refactor: build XML in a util

--- site/app/routes/episodes[.]xml.jsx
+++ site/app/routes/episodes[.]xml.jsx
@@ -1,38 +1,21 @@
-import { format } from 'date-fns';
-import { loadAllEpisodes } from '../util/load-all-episodes.server.js';
+import { createXML } from '~/util/create-xml.server.js';
+import { loadAllEpisodes } from '~/util/load-all-episodes.server.js';
 
 export const loader = async () => {
   const episodes = (await loadAllEpisodes()).slice(0, 20);
 
-  return new Response(
-    `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0"
-  xmlns:content="http://purl.org/rss/1.0/modules/content/"
-  xmlns:atom="http://www.w3.org/2005/Atom"
-  xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
->
-  <channel>
-    <title>Learn With Jason Episodes</title>
-    <description>Learn With Jason is live, hands-on learning with brilliant teachers from the web community every Tuesday &amp; Thursday. Join live and learn with us!</description>
-    <link>https://www.learnwithjason.dev/</link>
-    <atom:link href="https://www.learnwithjason.dev/episodes.xml" rel="self" type="application/rss+xml" />
-    <language>en-us</language>
-    <sy:updatePeriod>weekly</sy:updatePeriod>
-
-    ${episodes
-      .map(
-        (episode) => `
-    <item>
-      <title>${episode.title.replace(/&/g, '&amp;')}</title>
-      <pubDate>${format(
-        new Date(episode.date),
-        'eee, d E yyyy H:mm:ss XX',
-      )}</pubDate>
-      <description><![CDATA[<p>${episode.description.replace(
-        /&/g,
-        '&amp;',
-      )}</p>]]></description>
-      <content:encoded><![CDATA[
+  const rssFeed = createXML({
+    title: 'Learn With Jason Episodes',
+    description:
+      'Learn With Jason is live, hands-on learning with brilliant teachers from the web community every Tuesday &amp; Thursday. Join live and learn with us!',
+    siteUrl: 'https://www.learnwithjason.dev',
+    feedPath: '/episodes.xml',
+    items: episodes.map((episode) => ({
+      title: episode.title,
+      pubDate: episode.date,
+      description: episode.description,
+      link: `https://www.learnwithjason.dev/${episode.slug.current}`,
+      content: `
         <ul>
           <li>
             <a href="https://youtu.be/${
@@ -55,23 +38,14 @@ export const loader = async () => {
             )
             .join('')}
         </ul>
-      ]]></content:encoded>
-      <link>https://www.learnwithjason.dev/${episode.slug.current}</link>
-      <guid isPermaLink="true">https://www.learnwithjason.dev/${
-        episode.slug.current
-      }</guid>
-    </item>
-    `,
-      )
-      .join('')}
-  </channel>
-</rss>
-  `,
-    {
-      headers: {
-        'X-Boop': 'boop',
-        'Content-Type': 'application/xml',
-      },
+      `,
+    })),
+  });
+
+  return new Response(rssFeed, {
+    headers: {
+      'X-Boop': 'boop',
+      'Content-Type': 'application/xml',
     },
-  );
+  });
 };
