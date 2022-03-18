93989a1ee3c5a22afd9cd6da0d6b94987875cc37
Jason Lengstorf <jason@lengstorf.com>
2022-03-17 22:42:40 -0700

    feat: use new headshot in footer

--- site/app/components/footer.jsx
+++ site/app/components/footer.jsx
@@ -8,7 +8,7 @@ export function Footer() {
       <div className="host-bio">
         <div className="host-photo">
           <TeacherPhoto
-            imageURL="https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300,h_300,c_thumb,g_face,z_0.6/v1593462970/jason.af/jason-lengstorf-tokyo.jpg"
+            imageURL="https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_300,h_300,c_thumb,g_face,z_0.6/press/jason-lengstorf-ac-alley3.jpg"
             alt="Jason Lengstorf"
             width={125}
             skipFetch
