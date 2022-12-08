c2d82be03276119d1946abcb83a94756196d60ba
Jason Lengstorf <jason@lengstorf.com>
2022-12-07 17:47:27 -0800

    wip: Astro blog

---
+++ sites/blog/astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import theme from './config/shiki/theme-night-owl.json';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [mdx(), react()],
+	markdown: {
+		shikiConfig: {
+			theme,
+		},
+	},
+});
---
+++ sites/blog/src/pages/blog/edge-functions.md
@@ -0,0 +1,2 @@
+# edge_functions
+
