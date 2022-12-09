ab40c5a1fc622019a95643102a8fdc9f9f491423
Jason Lengstorf <jason@lengstorf.com>
2022-12-09 12:08:09 -0800

    feat: new scenes ready (except chat)

--- sites/scenes/src/routes/pair-programming.tsx
+++ sites/scenes/src/routes/pair-programming.tsx
@@ -1,4 +1,3 @@
-import { Bubble } from '@lwj/design-system/components/bubble';
 import { TwoUpVideo } from '../components/two-up-video';
 import styles from './pair-programming.module.css';
 
@@ -11,16 +10,6 @@ export function PairProgramming() {
 					<TwoUpVideo size="sidebar" />
 				</div>
 			</div>
-
-			<Bubble left={1312} top={15} diameter={35} />
-			<Bubble left={1859} top={99} diameter={50} />
-			<Bubble left={1316} top={379} diameter={15} />
-			<Bubble left={1790} top={380} diameter={120} />
-			<Bubble left={1301} top={725} diameter={80} />
-			<Bubble left={1886} top={502} diameter={25} />
-			<Bubble left={1316} top={820} diameter={15} />
-			<Bubble left={1843} top={795} diameter={35} />
-			<Bubble left={1890} top={828} diameter={15} />
 		</>
 	);
 }
