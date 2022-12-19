f8443cf5906a3976085bb24c85ce033abd481b8c
Jason Lengstorf <jason@lengstorf.com>
2022-12-18 22:43:17 -0800

    chore: update deps, whitespace

--- packages/design-system/components/search/highlight.jsx
+++ packages/design-system/components/search/highlight.jsx
@@ -2,18 +2,18 @@ import { Fragment } from 'react';
 import { parseAlgoliaHitHighlight } from '@algolia/autocomplete-preset-algolia';
 
 export function Highlight({ hit, attribute }) {
-  return (
-    <>
-      {parseAlgoliaHitHighlight({
-        hit,
-        attribute,
-      }).map(({ value, isHighlighted }, index) => {
-        if (isHighlighted) {
-          return <mark key={index}>{value}</mark>;
-        }
+	return (
+		<>
+			{parseAlgoliaHitHighlight({
+				hit,
+				attribute,
+			}).map(({ value, isHighlighted }, index) => {
+				if (isHighlighted) {
+					return <mark key={index}>{value}</mark>;
+				}
 
-        return <Fragment key={index}>{value}</Fragment>;
-      })}
-    </>
-  );
+				return <Fragment key={index}>{value}</Fragment>;
+			})}
+		</>
+	);
 }
