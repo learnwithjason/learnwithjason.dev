5a0a30b9726ef776451106e2de11d7adae11d249
Jason Lengstorf <jason@lengstorf.com>
2023-05-16 09:18:21 -0700

    wip: debugging the live player

--- sites/blog/src/components/live-player.tsx
+++ sites/blog/src/components/live-player.tsx
@@ -1,11 +1,12 @@
 import type { Episode } from '@lwj/types';
+
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
-import { Aside } from './aside';
 import { createEffect } from 'solid-js';
+import { Aside } from './aside';
 
 dayjs.extend(relativeTime);
 dayjs.extend(utc);
@@ -33,6 +34,8 @@ export const LivePlayer = (props: LivePlayerProps) => {
 		LIVE: start.isBefore(now) && end.isAfter(now),
 		OVER: end.isBefore(now),
 	};
+
+	console.log(now);
 
 	createEffect(() => {
 		if (!state.LIVE) {
