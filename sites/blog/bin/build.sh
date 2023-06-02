4416e7319dafdb8086e14239fc4d8c627fc8387c
Jason Lengstorf <jason@lengstorf.com>
2023-06-02 09:15:22 -0500

    feat: skip cache for Sanity webhooks

--- sites/blog/bin/build.sh
+++ sites/blog/bin/build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 echo "building..."
-echo "$INCOMING_HOOK_BODY"
+echo $INCOMING_HOOK_BODY
+if [ "$INCOMING_HOOK_BODY" = '{"skipCache":true}' ]; then
+  echo "skipping Nx cache"
+  nx run blog:build --skip-nx-cache
+else
+  nx run blog:build
+fi;
