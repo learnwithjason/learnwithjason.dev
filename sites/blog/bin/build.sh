a7100245a10a684945c7662eec324ef9b1c412f8
Jason Lengstorf <jason@lengstorf.com>
2023-06-02 09:29:51 -0500

    feat: clarified logging

--- sites/blog/bin/build.sh
+++ sites/blog/bin/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-echo "building..."
-echo $INCOMING_HOOK_BODY
+echo "Building the site..."
 if [ "$INCOMING_HOOK_BODY" = '{"skipCache":true}' ]; then
-  echo "skipping Nx cache"
+  echo "This build was triggered by a Sanity update. Skipping the Nx cache."
   nx run blog:build --skip-nx-cache
 else
   nx run blog:build
