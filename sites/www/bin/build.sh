a20eb96da40436b33d08f8137d8cb471e85530df
Jason Lengstorf <jason@lengstorf.com>
2023-06-26 12:08:52 -0700

    fix: update command

--- sites/www/bin/build.sh
+++ sites/www/bin/build.sh
@@ -3,7 +3,7 @@
 echo "Building the site..."
 if [ "$INCOMING_HOOK_BODY" = '{"skipCache":true}' ]; then
   echo "This build was triggered by a Sanity update. Skipping the Nx cache."
-  nx run blog:build --skip-nx-cache
+  nx run www:build --skip-nx-cache
 else
-  nx run blog:build
+  nx run www:build
 fi;
