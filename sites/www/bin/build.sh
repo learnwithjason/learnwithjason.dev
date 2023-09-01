e459a97346ac9c0516a60c766a590525a256c494
Jason Lengstorf <jason@lengstorf.com>
2023-08-31 17:44:48 -0700

    fix: hit it with a hammer

--- sites/www/bin/build.sh
+++ sites/www/bin/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 echo "Building the site..."
+
+# apparently Netlify skips optional deps now or something?
+npm i @nx/nx-linux-x64-gnu
+
 if [ "$INCOMING_HOOK_BODY" = '{"skipCache":true}' ]; then
   echo "This build was triggered by a Sanity update. Skipping the Nx cache."
   nx run www:build --skip-nx-cache
