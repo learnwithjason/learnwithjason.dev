e325c161a3471bc8452b8a76eb94bb5beb1cb027
Jason Lengstorf <jason@lengstorf.com>
2023-09-07 08:36:02 -0700

    post/workos sso (#79)
    * fix: typo
    
    * post: workos sso
    
    * chore: upgrade deps
    
    * chore: deps
    
    * feat: update sponsors on video cards
    
    * feat: add video, thumbnail
    
    * feat: update release date
    
    * chore: deps
    
    * fix: hit it with a hammer
    
    * chore: regenerate lockfile
    
    * feat: update poster sponsors
    
    * chore: upgrade deps
    
    * chore: regen lockfile
    
    * fix: remove extra build step
    
    * chore: use main branch lockfile

--- sites/www/bin/build.sh
+++ sites/www/bin/build.sh
@@ -2,9 +2,6 @@
 
 echo "Building the site..."
 
-# apparently Netlify skips optional deps now or something?
-npm i @nx/nx-linux-x64-gnu
-
 if [ "$INCOMING_HOOK_BODY" = '{"skipCache":true}' ]; then
   echo "This build was triggered by a Sanity update. Skipping the Nx cache."
   nx run www:build --skip-nx-cache
