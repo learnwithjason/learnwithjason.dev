defdcea173fe138d3e340a523c2a3ebae3d701ef
Jason Lengstorf <jason@lengstorf.com>
2024-01-13 14:12:41 -0800

    chore: TS

--- sites/api/util/addItemToCart.ts
+++ sites/api/util/addItemToCart.ts
@@ -1,8 +1,16 @@
 import { postToShopify } from './postToShopify';
 
-export const addItemToCart = async ({ cartId, itemId, quantity }) => {
+export const addItemToCart = async ({
+	cartId,
+	itemId,
+	quantity,
+}: {
+	cartId: string;
+	itemId: string;
+	quantity: number;
+}) => {
 	try {
-		const shopifyResponse = postToShopify({
+		const shopifyResponse = await postToShopify({
 			query: `
         mutation addItemToCart($cartId: ID!, $lines: [CartLineInput!]!) {
           cartLinesAdd(cartId: $cartId, lines: $lines) {
