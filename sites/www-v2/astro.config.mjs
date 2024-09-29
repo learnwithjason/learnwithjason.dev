e92393e28c93eec39ebc0903f8914d03a63cc51e
Jason Lengstorf <jason@lengstorf.com>
2024-09-29 10:56:06 -0700

    feat: update build command

--- sites/www-v2/astro.config.mjs
+++ sites/www-v2/astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://betas.learnwithjason.dev',
+	site: 'https://beta.learnwithjason.dev',
 	output: 'server',
 	integrations: [
 		clerk({
--- sites/www/bin/build.sh
+++ sites/www/bin/build.sh
@@ -4,9 +4,9 @@ echo "Building the site..."
 
 if [ "$INCOMING_HOOK_BODY" = '{"skipCache":true}' ]; then
   echo "This build was triggered by a Sanity update. Skipping the Nx cache."
-  nx run www:build --skip-nx-cache
+  nx run www-v2:build --skip-nx-cache
 else
-  nx run www:build
+  nx run www-v2:build
 fi;
 
 exit $?
