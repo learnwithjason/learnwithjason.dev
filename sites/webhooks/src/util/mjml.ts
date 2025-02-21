5ea5584e9295043eb4c31f43c4186ec4059745b9
Jason Lengstorf <jason@lengstorf.com>
2025-02-20 17:44:31 -0800

    feat: update newsletter template

--- sites/webhooks/src/util/mjml.ts
+++ sites/webhooks/src/util/mjml.ts
@@ -40,93 +40,93 @@ function getUtmLink({
 	return utmUrl.toString();
 }
 
-async function getSchedule({ utm_campaign }: { utm_campaign: string }) {
-	const date = new Date();
-	const res = await fetch('https://www.learnwithjason.dev/api/v2/schedule');
+// async function getSchedule({ utm_campaign }: { utm_campaign: string }) {
+// 	const date = new Date();
+// 	const res = await fetch('https://www.learnwithjason.dev/api/v2/schedule');
 
-	if (!res.ok) {
-		console.error(res);
-		return [];
-	}
+// 	if (!res.ok) {
+// 		console.error(res);
+// 		return [];
+// 	}
 
-	const schedule = await res.json();
-	console.log(schedule);
+// 	const schedule = await res.json();
+// 	console.log(schedule);
 
-	const formatted = schedule
-		.filter(
-			(ep: any) =>
-				new Date(ep.date) >= date && ep.guest.name !== 'Jason Lengstorf'
-		)
-		.slice(0, 4)
-		.map((episode: any, index: number) => {
-			const bottomPadding = index === 3 ? '8px' : '4px';
-			const episodeDate = new Date(episode.date).toLocaleDateString('en-US', {
-				month: 'short',
-				day: 'numeric',
-			});
-			const link = getUtmLink({
-				url: episode.uri,
-				campaign: utm_campaign,
-				medium: 'schedule-link',
-			});
+// 	const formatted = schedule
+// 		.filter(
+// 			(ep: any) =>
+// 				new Date(ep.date) >= date && ep.guest.name !== 'Jason Lengstorf',
+// 		)
+// 		.slice(0, 4)
+// 		.map((episode: any, index: number) => {
+// 			const bottomPadding = index === 3 ? '8px' : '4px';
+// 			const episodeDate = new Date(episode.date).toLocaleDateString('en-US', {
+// 				month: 'short',
+// 				day: 'numeric',
+// 			});
+// 			const link = getUtmLink({
+// 				url: episode.uri,
+// 				campaign: utm_campaign,
+// 				medium: 'schedule-link',
+// 			});
 
-			return `
-				<tr>
-					<td style="padding: 8px 5px ${bottomPadding} 0">${episodeDate}</td>
-					<td style="padding: 8px 0 ${bottomPadding}">
-						<img
-							src="https://res.cloudinary.com/jlengstorf/image/fetch/w_70,h_70,c_fill,g_faces,f_auto,q_auto/${episode.guest.image}"
-							alt="${episode.guest.name}"
-							width="35px"
-							style="border-radius: 50%; vertical-align: bottom;"
-						/>
-					</td>
-					<td style="padding: 8px 5px ${bottomPadding} 0">
-					${episode.guest.name}
-					</td>
-					<td style="padding: 8px 0 ${bottomPadding}"><a href="${link}">${episode.title}</a></th>
-				</tr>
-			`;
-		})
-		.join('');
+// 			return `
+// 				<tr>
+// 					<td style="padding: 8px 5px ${bottomPadding} 0">${episodeDate}</td>
+// 					<td style="padding: 8px 0 ${bottomPadding}">
+// 						<img
+// 							src="https://res.cloudinary.com/jlengstorf/image/fetch/w_70,h_70,c_fill,g_faces,f_auto,q_auto/${episode.guest.image}"
+// 							alt="${episode.guest.name}"
+// 							width="35px"
+// 							style="border-radius: 50%; vertical-align: bottom;"
+// 						/>
+// 					</td>
+// 					<td style="padding: 8px 5px ${bottomPadding} 0">
+// 					${episode.guest.name}
+// 					</td>
+// 					<td style="padding: 8px 0 ${bottomPadding}"><a href="${link}">${episode.title}</a></th>
+// 				</tr>
+// 			`;
+// 		})
+// 		.join('');
 
-	if (formatted.length < 1) {
-		return '';
-	}
+// 	if (formatted.length < 1) {
+// 		return '';
+// 	}
 
-	const mjml = `
-		<!-- LWJ SCHEDULE -->
-		<mj-section>
-			<mj-column>
-				<mj-spacer height="20px" />
-				<mj-text mj-class="heading">
-					<h2>Upcoming LWJ episodes:</h2>
-				</mj-text>
-			</mj-column>
-		</mj-section>
+// 	const mjml = `
+// 		<!-- LWJ SCHEDULE -->
+// 		<mj-section>
+// 			<mj-column>
+// 				<mj-spacer height="20px" />
+// 				<mj-text mj-class="heading">
+// 					<h2>Upcoming Learn With Jason episodes:</h2>
+// 				</mj-text>
+// 			</mj-column>
+// 		</mj-section>
 
-		<mj-section>
-			<mj-column>
-				<mj-table font-size="13px">
-					<tr style="text-align: left; border-bottom: 1px solid;">
-						<th width="55px" style="font-size: 11px; font-weight: normal; padding: 4px 0;">Date</th>
-						<th width="40px" style="font-size: 11px; font-weight: normal; padding: 4px 0;">Expert</th>
-						<th width="100px" style="font-size: 11px; font-weight: normal; padding: 4px 0;"></th>
-						<th style="font-size: 11px; font-weight: normal; padding: 4px 0;">Topic</th>
-					</tr>
-					${formatted}
-					<tr>
-						<td colspan="4" style="padding: 8px 0 0; font-size: 12px; border-top: 1px solid;">
-							Visit <a href="https://www.learnwithjason.dev/schedule">lwj.dev/schedule</a> to see all upcoming episodes.
-						</td>
-					</tr>
-				</mj-table>
-			</mj-column>
-		</mj-section>
-	`;
+// 		<mj-section>
+// 			<mj-column>
+// 				<mj-table font-size="13px">
+// 					<tr style="text-align: left; border-bottom: 1px solid;">
+// 						<th width="55px" style="font-size: 11px; font-weight: normal; padding: 4px 0;">Date</th>
+// 						<th width="40px" style="font-size: 11px; font-weight: normal; padding: 4px 0;">Expert</th>
+// 						<th width="100px" style="font-size: 11px; font-weight: normal; padding: 4px 0;"></th>
+// 						<th style="font-size: 11px; font-weight: normal; padding: 4px 0;">Topic</th>
+// 					</tr>
+// 					${formatted}
+// 					<tr>
+// 						<td colspan="4" style="padding: 8px 0 0; font-size: 12px; border-top: 1px solid;">
+// 							Visit <a href="https://codetv.dev/series/learn-with-jason/s8">the series home page</a> to see all episodes.
+// 						</td>
+// 					</tr>
+// 				</mj-table>
+// 			</mj-column>
+// 		</mj-section>
+// 	`;
 
-	return mjml;
-}
+// 	return mjml;
+// }
 
 async function getFeaturedContent({
 	featuredItems,
@@ -160,16 +160,7 @@ async function getFeaturedContent({
 		})
 		.join('');
 
-	const mjml = `<!-- LWJ CONTENT -->
-	<mj-section>
-		<mj-column>
-			<mj-spacer height="40px" />
-			<mj-text mj-class="heading">
-				<h2>Learn With Jason around the web:</h2>
-			</mj-text>
-		</mj-column>
-	</mj-section>
-
+	const mjml = `
 	<mj-section padding-bottom="20px">
 		${formatted}
 	</mj-section>
@@ -190,7 +181,7 @@ export async function getNewsletterTemplateMarkup({
 		.replace(/\s+/g, '-')
 		.replace(/^-+|[^a-z0-9-\s]|-+$/gi, '')
 		.replace(/-+/g, '-');
-	const schedule = await getSchedule({ utm_campaign });
+	// const schedule = await getSchedule({ utm_campaign });
 	const featured = await getFeaturedContent({ featuredItems, utm_campaign });
 	const mjml = `<mjml lang="en">
 	<mj-head>
@@ -246,8 +237,7 @@ export async function getNewsletterTemplateMarkup({
 		<!-- HEADER -->
 		<mj-section>
 			<mj-column>
-				<mj-image width="120px" src="https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_240/v1579281727/lwj/learn-with-jason.png" alt="Learn With Jason"></mj-image>
-				<mj-divider border-width="1px" />
+				<mj-image src="https://res.cloudinary.com/jlengstorf/image/upload/f_auto/q_auto/v1739239348/codetv/newsletter-banner.png" alt="CodeTV: tv for developers"></mj-image>
 			</mj-column>
 		</mj-section>
 
@@ -260,29 +250,27 @@ export async function getNewsletterTemplateMarkup({
 			</mj-column>
 		</mj-section>
 
+		${featured}
+
 		<mj-section>
 			<mj-column>
 				<mj-image
-					src="https://res.cloudinary.com/jlengstorf/image/upload/f_auto,q_auto/v1684008918/lwj/discord-newsletter-banner.jpg"
-					alt="Want to connect with a community of devs who love to learn and build? Join the LWJ Discord!"
-					href="https://discord.gg/lwj"
+					src="https://res.cloudinary.com/jlengstorf/image/upload/f_auto/q_auto/v1739239345/codetv/support-banner.png"
+					alt="Early access to new episodes and directly fund new shows. Click here to support CodeTV."
+					href="https://codetv.dev/support"
 				/>
 			</mj-column>
 		</mj-section>
-
-		${featured}
-
-		${schedule}
 
 		<!-- FOOTER -->
 		<mj-section css-class="footer">
 			<mj-column padding-top="40px">
 				<mj-text mj-class="footer">
 					<p>
-						This message contains no gluten or sulfites. So if it gives you a headache or a tummyache it&lsquo;s either because the content is so good it hurts, or because I&lsquo;m such a doofus that you&lsquo;re taking psychic damage. So... you&lsquo;re welcome. Or, I&lsquo;m sorry.
+						This message contains no gluten, sulfites, parabens, or microplastics. If it gives you a headache or a tummyache it&rsquo;s either because the content is so good it hurts&hellip; or because we&rsquo;re such dorks it&rsquo;s causing psychic damage. So... you&rsquo;re welcome. Or we&rsquo;re sorry.
 					</p>
 					<p>
-						You&lsquo;re receiving this because you subscribed at either <a href="https://www.learnwithjason.dev">learnwithjason.dev</a>, <a href="https://jason.energy">jason.energy</a>, or one of Jason Lengstorf&lsquo;s other web properties. If you&lsquo;d like to stop receiving these, you can <a href="{{ unsubscribe_url }}">unsubscribe</a> any time.
+						You&rsquo;re receiving this because you subscribed at codetv.dev. Or, if you&rsquo;ve been here a while, you may have subscribed at learnwithjason.dev or jason.energy or even — if you&rsquo;re a real OG — at lengstorf.com. If you&rsquo;d like to stop receiving these, you can <a href="{{ unsubscribe_url }}">unsubscribe</a> any time.
 					</p>
 					<p>
 						{{ address }}
